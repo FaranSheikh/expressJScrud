77207bee1f6d2ba9840077e48f81cf5be24da6ab
FaranSheikh <faran.sheikh313@gmail.com>
2022-11-19 14:02:02 +0530

    add user.sql file

---
+++ database/user.sql
@@ -0,0 +1,5 @@
+-- INNER JOIN Tables | Faran  | 19 Nov, 2022 
+SELECT * FROM students INNER JOIN schools ON students.id = schools.stud_id
+
+-- LEFT JOIN Tables | Faran  | 19 Nov, 2022 
+SELECT * FROM students LEFT JOIN schools ON students.id = schools.stud_id
